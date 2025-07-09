e3ed13f057dcd33f1b28e754b76cf4f067ae39b1
maxim <borisov130490@gmail.com>
2025-07-09 19:16:00 +0300

    add search by slug

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -2,6 +2,32 @@
  * product controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController(
+  "api::product.product",
+  ({ strapi }) => ({
+    async findBySlug(ctx) {
+      const { slug } = ctx.params;
+
+      try {
+        // Ищем заказ по slug
+        const entity = await strapi.entityService.findMany(
+          "api::product.product",
+          {
+            filters: { slug },
+            limit: 1,
+            populate: "*",
+          }
+        );
+
+        ctx.body = entity[0] || null;
+
+        return (ctx.body = entity[0] || null);
+      } catch (error) {
+        strapi.log.error("Error in findOne product:", error);
+        return ctx.internalServerError("Something went wrong.");
+      }
+    },
+  })
+);
---
+++ src/api/product/routes/slugSearch.ts
@@ -0,0 +1,9 @@
+export default {
+  routes: [
+    {
+      method: "GET",
+      path: "/product/:slug",
+      handler: "product.findBySlug",
+    },
+  ],
+};
